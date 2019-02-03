2feb16b2a5236aea631ea39c108faeb5eb7b64ac
wputthividhya <wputthividhya@gmail.com>
2019-02-03 21:11:28 +0700

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -9,5 +9,5 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
-    posts = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'posts':posts})
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
